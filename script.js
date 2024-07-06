015b1ad8c38915e852e47bbfd76c0da58ecded60
pacestefano <135709394+pacestefano@users.noreply.github.com>
2024-07-06 13:58:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function startGame(game) {
+    if (game === 'gioco1') {
+        window.location.href = 'gioco1/index.html';
+    } else if (game === 'gioco2') {
+        window.location.href = 'gioco2/index.html';
+    }
+}
